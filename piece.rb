b91a78d98bfe3ced7a6a51ed2d9a355a5526d0f1
Ben Smith and Andrew Richards <student@appacademy.io>
2014-02-04 10:32:29 -0800

    break for employees

--- piece.rb
+++ piece.rb
@@ -7,14 +7,14 @@ class Piece
   end
 
   def move(target_pos)
-    valid_move_endpoints = []
-    if valid_move_endpoints.include?(target_pos)
+    if valid_move?(target_pos)
       capture_piece(target_pos)
       self.pos = target_pos
       @board[pos] = self
     else raise RuntimeError.new("This is not a valid move position")
+    end
+    nil
   end
-  nil
 
   def capture_piece(pos)
     captured_piece = nil
@@ -24,4 +24,55 @@ class Piece
     end
     captured_piece
   end
+
+  def valid_move?(pos)
+    # is it on the board
+    # columns are letters
+    # rows are numbers
+    x, y = pos
+
+    [x, y].all? { }
+    # pos empty or pos contains enemy piece - OK!
+    # pos contains friendly piece? - not OK
+  end
 end
+
+class SlidingPiece < Piece
+  HORIZ = [[1, 0], [0, 1]]
+  DIAG = [[1, 1], [1, -1]]
+
+  def valid_move?(target_pos)
+    # check horiz / diag positions
+    super(target_pos)
+  end
+end
+
+class Bishop < SlidingPiece
+end
+
+class Rook < SlidingPiece
+end
+
+class Queen < SlidingPiece
+end
+
+class SteppingPiece < Piece
+  OFFSETS = [[]]
+  def valid_move?(target_pos)
+    # check if valid move coord
+    super(target_pos)
+  end
+
+end
+
+class Knight < SteppingPiece
+end
+
+class King < SteppingPiece
+end
+
+# to do
+class Pawn
+end
+
+
