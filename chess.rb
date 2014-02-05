7096df2a7e95ab87be73f7d62d27da1105a67a92
Ben Smith and Andrew Richards <student@appacademy.io>
2014-02-05 12:03:35 -0800

    added save feature (untested)

--- chess.rb
+++ chess.rb
@@ -10,7 +10,6 @@ require "colorize"
 # TODO:
 # draw conditions
 # movement special cases: en passant, castling, pawn promotion
-# piece value
 # computer player
 # save / load; replays?
 # source code cleanup - finish splitting classes, decomposition, line-tightening, privatization
@@ -27,6 +26,7 @@ class Game
     @current_player = @white_player
     @winner = nil
     @move_history = [@board.dup]
+    new_or_load
   end
 
   def associate_player(player, color)
@@ -37,41 +37,47 @@ class Game
   end
 
   def play
-    forfeit = catch(:forfeit) do
-      puts @board
-
-      until over?
-        begin
-          next_player = (@current_player == @white_player ? @black_player : @white_player)
-
-          start_pos, end_pos = @current_player.play_turn
-          validate_start_pos(start_pos)
-          @board[start_pos].move(end_pos)
-        rescue InvalidInputError => e
-          puts e
-          retry
-        rescue InvalidMoveError => e
-          puts e
-          retry
-        end
-
+    save = catch(:save) do
+      forfeit = catch(:forfeit) do
         puts @board
 
-        if @board.in_check?(next_player.color)
-          if @board.checkmate?(next_player.color)
-            @winner = @current_player
+        until over?
+          begin
+            next_player = (@current_player == @white_player ? @black_player : @white_player)
+
+            start_pos, end_pos = @current_player.play_turn
+            validate_start_pos(start_pos)
+            @board[start_pos].move(end_pos)
+          rescue InvalidInputError => e
+            puts e
+            retry
+          rescue InvalidMoveError => e
+            puts e
+            retry
           end
+
+          puts @board
+
+          if @board.in_check?(next_player.color)
+            if @board.checkmate?(next_player.color)
+              @winner = @current_player
+            end
+          end
+
+          @current_player = next_player
         end
-
-        @current_player = next_player
       end
+
+      if forfeit
+        @winner = (forfeit == @white_player ? @black_player : @white_player)
+      end
+
+      puts "Game over - #{@winner} is the winner!"
     end
 
-    if forfeit
-      @winner = (forfeit == @white_player ? @black_player : @white_player)
+    if save
+      self.save_game
     end
-
-    puts "Game over - #{@winner} is the winner!"
   end
 
   def validate_start_pos(start_pos)
@@ -86,6 +92,31 @@ class Game
     !@winner.nil?
   end
 
+  def new_or_load
+    # start new game? or load previous game?
+    # if start new - self.play
+  end
+
+  def save_game
+    begin
+      puts "Please enter a name for this game. (Press enter for default.)"
+      game_name = gets.chomp
+      if game_name.match(/[^\w]/).nil?
+        raise InvalidInputError.new("Error - Please use only alphanumeric characters and underscores.")
+      end
+    rescue InvalidInputError => e
+      puts e
+      retry
+
+      game_name = "chess_game" if game_name == ""
+
+      File.open("#{game_name}_#{Time.now}.txt", "w") { self.to_yaml }
+    end
+
+  end
+
+  def load_game
+  end
 end
 
 if __FILE__ == $PROGRAM_NAME
